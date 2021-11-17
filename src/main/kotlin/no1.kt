b7f8f72cfd187217a89adfc7da0afc48763012b6
reziqvins <nreziq@gmail.com>
2021-11-17 16:21:37 +0700

    pengkondisian

--- src/main/kotlin/no1.kt
+++ src/main/kotlin/no1.kt
@@ -1,25 +1,28 @@
 fun main () {
     //Deklarasi variable dan tioe datanya
-    val belanja: Double = 400000.0;
-    var member = false;
+    val belanja: Double = 625000.0;
+    var member = true;
     var potMem = 5 / 100
     var pot = 10 / 100
     var disc15 = belanja * 15 / 100
     var disc5 = belanja * 5 / 100
     var disc10 = belanja * 10 / 100
+    var aku =("pembeli 1 mendapat diskon 15% dari")
+
+
 
     if (member == true) {
         if (belanja >= 500000.0) {
-            println("Total Belanja1 = " + (belanja - disc15))
+            println("pembeli 1 mendapat diskon 15% dari $belanja jadi totalnya = $belanja - $disc15 = " + (belanja - disc15))
         } else {
-            println("total belanja2 =" + (belanja - disc5))
+            println("pembeli 1 mendapat diskon 5% dari $belanja jadi totalnya = $belanja - $disc5 = " + (belanja - disc5))
         }
 
     } else {
         if (belanja >= 500000.0) {
-            println("total belanja3= " + (belanja - disc10))
+            println("pembeli 1 mendapat diskon 10% dari $belanja jadi totalnya = $belanja - $disc10 = "+ (belanja - disc10))
         } else {
-            println("total belanjja4= " + belanja)
+            println("pembeli 1 mmembayar dengan total = "+ belanja)
         }
     }
 }
